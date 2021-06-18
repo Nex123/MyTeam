ca80fe5e3e0024e74be50ca1c9c64c16522c26c7
schueler <schueler@ubuntu>
2021-06-18 11:12:31 +0200

    Pls work

--- 13aMyTeam/src/pkgMain/MainAppl.java
+++ 13aMyTeam/src/pkgMain/MainAppl.java
@@ -6,7 +6,7 @@ public class MainAppl {
 
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
-		System.out.println(player);
+		System.out.println("==" + player);
 	}
 
 }
